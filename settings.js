9a0606899b720cbd02a0dfff48d6a6bf418f4362
AlvinEdwards <128837558+AlvinEdwards@users.noreply.github.com>
2024-12-02 16:48:00 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "234704103936"
+global.pairingNumber = "256740761661"
  
-global.botNumber = ["234704103936"]
+global.botNumber = ["256740761661"]
 
 global.sign = "✘"
 // ☞ ➻ ➽ ☛ 
